4c8c3905f6509cb255fa370950fd97314a682ff5
simegnew-m <simegnew.misganaw@gmail.com>
2022-05-07 15:33:15 -0700

    # web_flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Starts a Flask web application.
-The application listens on 0.0.0.0, port 5000.
+The application listens on 18.204.48.246 , port 5000.
 Routes:
     /: Displays 'Hello HBNB!'
 """
